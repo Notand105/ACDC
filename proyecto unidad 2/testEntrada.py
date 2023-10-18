88e3b6f1187ba52cc90ef8054c2db2ecf1ef7146
Notand105 <urzua.fernando.f@gmail.com>
2023-10-18 14:44:39 -0300

    falta escrituras

--- proyecto unidad 2/testEntrada.py
+++ proyecto unidad 2/testEntrada.py
@@ -1,8 +1,40 @@
 import argparse
 import sys
 import re
+import math 
+
+#Configuración
+blockSize = 2
+cacheSize = 2
+offset = 0
+line = 0
+tag = 0
+writeThrough = False
+fullyAssociative = False
+setAssociative = 0
+noAllocate = False
+splitCache = False
+
+#estadisticas
+missesDatos = 0
+hitsDatos = 0
+missesInstrucciones = 0
+hitsInstrucciones = 0
+lineas_a_analizar = 0
+wordsCopiados = 0
+wordsEscritos = 0
+
+def convertir_a_binario(cadena):
+    aDecimal = int(cadena, 16)
+    binario = bin(aDecimal)[2:]
+    if len(binario) < 32:
+        binario = "0" * (32 - len(binario)) + binario 
+    return binario
 
 def configurar_cache():
+    
+    global blockSize,cacheSize,writeThrough, fullyAssociative,setAssociative ,noAllocate ,splitCache 
+
     parser = argparse.ArgumentParser(description="Configuración de la caché")
 
     parser.add_argument("-bs", type=int, help="Tamaño de bloque en palabras")
@@ -21,21 +53,32 @@ def configurar_cache():
     # Imprimir la configuración
     print("Configuración de la caché:")
     if args.bs:
-        print(f"Tamaño de bloque: {args.bs} palabras")
+        blockSize = args.bs
+        print(f"Tamaño de bloque: {blockSize} palabras")
     if args.cs:
-        print(f"Tamaño del caché: {args.cs} líneas")
+        cacheSize = args.cs
+        print(f"Tamaño del caché: {cacheSize} líneas")
     if args.wt:
+        writeThrough = True
         print("Cache Write-Through")
     else:
         print("Cache Write-Back (por defecto)")
     if args.fa:
+        fullyAssociative = True
         print("Cache Fully Associative")
     if args.sa:
+        setAssociative = args.sa
         print(f"Cache Set-Associative: {args.sa} sets")
     if args.wna:
+        noAllocate = True
         print("Cache Write-No-Allocate (por defecto, Write-Allocate)")
     if args.split:
+        splitCache = True
         print("Cache separado en dos secciones para instrucciones y datos")
+
+    if (setAssociative and fullyAssociative):
+        setAssociative = False
+        fullyAssociative = False
 
     # Procesar los datos del archivo.trace
     print("Datos del archivo.trace:")
@@ -49,10 +92,115 @@ def configurar_cache():
         newLine = [splitedLine[0],splitedLine[1]]
         #quitamos los saltos de linea en las direcciones de memoria
         newLine[1] = re.sub('\n', '', newLine[1])
+        #llenamos new line[1] con ceros si este es menor a 8
+        if (len(newLine[1])<8):
+            ceros = 8 - len(newLine[1])
+            newLine[1] = "0"*ceros + newLine[1]
+        #convertimos la direccion a binario
+        newLine[1] = convertir_a_binario(newLine[1])
         instructions.append(newLine)
 
-    for i in instructions:
-        print(i)
+    return instructions
+
+def dividir_entrada():
+    global offset, line, tag, blockSize, cacheSize
+    offset = int(math.log(blockSize, 2))
+    line = int(math.log(cacheSize, 2))
+    #asumimos todas las entradas de 32 bits
+    tag = 32 - (offset + line )
+
+def creaCache():
+    #tenemos que crear un cache de cacheSize lineas
+    global cacheSize 
+    cache = []
+    for i in range(cacheSize):
+        cache.append('empty')
+    return cache
+
+def mapeo_directo(cache, instruccion):
+    global offset, line, tag
+    ##addr[0] es el tag , addr[1] es la linea en la que guardarla
+    addr = format_instructions(instruccion[1])
+    lineInCache = int(addr[1],2)
+    if (instruccion[0] == '0'):
+        print("leer dato")
+        leerDatoMD(cache, lineInCache, addr[0])
+    elif(instruccion[0]=='1'):
+        print('escribir dato')
+    elif (instruccion[0])=='2':
+        print('leer instruccion')
+        leerInstruccionMD(cache, lineInCache, addr[0])
+    return cache
+
+def leerDatoMD(cache, line, tag):
+    global missesDatos, hitsDatos, wordsCopiados, blockSize
+    if (cache[line] == 'empty'):
+        print("miss por no existir, lo traemos de memoria")
+        #agrergar logica para escribir
+        cache[line] = tag
+        missesDatos += 1
+        wordsCopiados += blockSize 
+    elif(cache[line] != tag  ):
+        print("miss por existir otro, reemplazamos por el nuevo trayendolo desde memoria")
+        #agrergar logica para escribir
+        cache[line] = tag
+        missesDatos += 1
+        wordsCopiados += blockSize 
+    elif (cache[line] == tag):
+        print("hit")
+        hitsDatos += 1
+
+    return cache
+
+def leerInstruccionMD(cache, line, tag):
+    global missesInstrucciones, hitsInstrucciones, wordsCopiados, blockSize
+    if (cache[line] == 'empty'):
+        print("miss por no existir, lo traemos de memoria")
+        #agrergar logica para escribir
+        cache[line] = tag
+        missesInstrucciones += 1
+        wordsCopiados += blockSize 
+    elif(cache[line] != tag  ):
+        print("miss por existir otro, reemplazamos por el nuevo trayendolo desde memoria")
+        #agrergar logica para escribir
+        cache[line] = tag
+        missesInstrucciones += 1
+        wordsCopiados += blockSize 
+    elif (cache[line] == tag):
+        print("hit")
+        hitsInstrucciones += 1
+
+    return cache
+
+
+
+
+def format_instructions(instruction):
+    global offset,tag,line
+
+    itag = instruction[:tag] 
+    iline = instruction[tag:tag + line]
+    return [itag,iline] 
+
+def print_stats():
+    global missesDatos, hitsDatos, missesInstrucciones, hitsInstrucciones
+    print("referencias a Instrucciones: ", missesInstrucciones+ hitsInstrucciones)
+    print("referencias a datos: ", missesDatos + hitsDatos)
+    print("misses datos: ", missesDatos)
+    print("misses Instrucciones: ", missesInstrucciones)
+    print("palabras copiadas desde memoria hasta caché: ", wordsCopiados)
+    print("palabras escritas desde caché a memoria: ", wordsEscritos)
+    tiempo = (hitsDatos * 5) + (hitsInstrucciones * 5) + (wordsCopiados * 100) + (wordsEscritos * 100)
+    print("tiempo: ", tiempo, " ns")
+
+
 
 if __name__ == "__main__":
-    configurar_cache()
+    instructions = configurar_cache()
+    dividir_entrada() 
+    cache = creaCache()
+    for i in instructions:
+        cache = mapeo_directo(cache, i)
+
+    print_stats() 
+    
